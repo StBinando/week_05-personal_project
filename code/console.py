c47821b5c561f6ce3bd7c1d1da1f07d83f7048dc
Stefano Binando <sbinando@gmail.com>
2021-11-12 16:15:26 +0000

    creates album_repository with functions: add_album show_all select_1_album_by_id delete_1_album_by_id

--- code/console.py
+++ code/console.py
@@ -8,6 +8,7 @@ from models.pre_order import PreOrder
 
 
 import repositories.artist_repository as artist_repository
+import repositories.album_repository as album_repository
 
 import os
 os.system('psql -d rubberduck_records -f db/rubberduck_records.sql')
@@ -16,6 +17,7 @@ os.system('psql -d rubberduck_records -f db/rubberduck_records.sql')
 artist_1 = Artist("David Bowie")
 artist_2 = Artist("Iggy Pop")
 album_1 = Album(artist_1, "Low")
+album_2 = Album(artist_1, "Pin-Up")
 item_1 = Item(album_1, "CD", 6.50, 9.99, 7, 2, 1)
 customer_1 = Customer("John Smith", "+447857634091")
 pre_order_1 = PreOrder(customer_1, item_1)
@@ -44,3 +46,25 @@ results = artist_repository.show_all()
 for row in results:
     print(row.__dict__)
 
+print("------------- ADDS 1st ALBUM ----------------")
+result = album_repository.add_album(album_1)
+print(result.__dict__)
+
+print("------------- ADDS 2nd ALBUM ----------------")
+result = album_repository.add_album(album_2)
+print(result.__dict__)
+
+print("------------ PRINT ALL ALBUMS -------------")
+results = album_repository.show_all()
+for row in results:
+    print(row.__dict__)
+
+print("------------ PRINT 2nd ALBUM -------------")
+result = album_repository.select_1_album_by_id(2)
+print(result.__dict__)
+
+print("------------ DELETES 2nd ALBUM --------------")
+album_repository.delete_1_album_by_id(2)
+results = album_repository.show_all()
+for row in results:
+    print(row.__dict__)
---
+++ code/repositories/album_repository.py
@@ -0,0 +1,38 @@
+from db.run_sql import run_sql
+from models.artist import Artist
+from models.album import Album
+
+import repositories.artist_repository as artist_repository
+
+
+def add_album(album):
+    sql = "INSERT INTO albums (artist_id, title) VALUES (%s, %s) RETURNING *"
+    values = [album.artist.id, album.title]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    album.id = id
+    return album
+
+def show_all():
+    albums = []
+    sql = "SELECT * FROM albums"
+    results = run_sql(sql)
+    for row in results:
+        artist = artist_repository.select_1_artist_by_id(row['artist_id'])
+        album = Album(artist, row['title'], row['id'])
+        albums.append(album)
+    return albums
+
+def select_1_album_by_id(id):
+    sql = "SELECT * FROM albums where id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    # artist = Artist(result['name'], result['id'])
+    artist = artist_repository.select_1_artist_by_id(result['artist_id'])
+    album = Album(artist, result['title'], result['id'])
+    return album
+
+def delete_1_album_by_id(id):
+    sql = "DELETE FROM albums WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
